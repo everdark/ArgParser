420dbb2142e5db5d16176291a1770d071dc76b6f
Kyle Chung <kylechun9@gmail.com>
2015-10-21 19:53:09 +0800

    update test template

--- test.R
+++ test.R
@@ -3,10 +3,12 @@
 library(methods)
 library(magrittr)
 source("ArgParser.R")
-p <- ArgParser() %>% addFlag("--f1") %>%
-    addFlag("--f2", optional=FALSE) %>%
-    addSwitch("--s1", default=FALSE) %>%
-    addSwitch("--s2", default=list(1,0))
+p <- ArgParser(desc="A test for ArgParser") %>% 
+    addFlag("--f1", help="this is an optional flag") %>%
+    addFlag("--f2", help="this is an optional flag with default value", default="f1d") %>%
+    addFlag("--f3", help="this is a forced flag", optional=FALSE) %>%
+    addSwitch("--s1", help="a logical switch", default=FALSE) %>%
+    addSwitch("--s2", help="an ad-hoc switch", default=list(1,0))
 pargs <- parseCommandLine(p, commandArgs())
 for ( arg in names(pargs) )
     write(paste(arg, pargs[[arg]], '\t'), stdout())
