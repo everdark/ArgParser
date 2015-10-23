8f1176ca1cb5ac5c615ede7b7ba8cabba94c80a2
Kyle Chung <kylechun9@gmail.com>
2015-10-23 12:16:37 +0800

    correct a bug in validiy checking for alias

--- ArgParser.R
+++ ArgParser.R
@@ -20,13 +20,13 @@ ArgParser <- setClass("ArgParser",
                           all_argnames <- c(names(object@switches_logic), 
                                             names(object@switches_any), 
                                             names(object@flags))
-                          all_alias <- c(names(object@flags_alias),
-                                         names(object@switches_alias))
+                          all_alias <- c(object@flags_alias,
+                                         object@switches_alias)
                           if ( any(sapply(all_argnames, function(x) substr(x,1,2) != "--")) )
                               return("Name of flags/switches should have double-dash (--) prefix.")
-                          if ( any(duplicated(all_argnames)) || any(duplicated(all_alias)) )
+                          if ( any(duplicated(c(all_argnames, all_alias))) )
                               return("Duplicated flags/switches found.")
-                          if ( any(sapply(all_alias, function(x) substr(x,1,1) != '-')) )
+                          if ( any(sapply(all_alias, function(x) substr(x,1,1) != '-'), na.rm=TRUE) )
                               return("Short name alias should have single-dash (-) prefix.")
                           TRUE
                       })
