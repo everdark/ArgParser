6281b203a194efef528066050365afb208931746
Kyle Chung <kylechun9@gmail.com>
2015-10-23 16:35:51 +0800

    update test template

--- test.R
+++ test.R
@@ -6,10 +6,12 @@ source("ArgParser.R")
 p <- ArgParser(desc="A test for ArgParser") %>% 
     addFlag("--flag1", "-f1", help="this is an optional flag") %>%
     addFlag("--flag2", help="this is an optional flag with default value", default="f1d") %>%
-    addFlag("--flag3", help="this is a forced flag", optional=FALSE) %>%
+    addFlag("--flag3", "-f3", help="this is a forced flag", optional=FALSE) %>%
     addSwitch("--logical-switch", "-s1", help="a logical switch", states=FALSE) %>%
-    addSwitch("--adhoc-switch", "-s2", help="an ad-hoc switch", states=list(1,0))
-pargs <- parseCommandLine(p, commandArgs())
+    addSwitch("--adhoc-switch", "-s2", help="an ad-hoc switch", states=list(1,0)) %>%
+    addOpt("opt1", help="positional arg 1") %>%
+    addOpt("opt2", help="positional arg 2")
+pargs <- parseCommandLine(p, commandArgs(), trim=T)
 for ( arg in names(pargs) )
     write(paste(arg, pargs[[arg]], '\t'), stdout())
 
