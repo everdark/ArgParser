5c73c09e48075bc8bf8b348bb6f8abb2e61b7a87
Kyle Chung <kylechun9@gmail.com>
2015-10-26 19:13:20 +0800

    remove top-level script

--- run-unittests.R
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env Rscript
-
-library(methods)
-library(testthat)
-library(magrittr)
-
-invisible(sapply(dir("./R", full.names=TRUE), source))
-
-test_dir("tests/testthat", reporter="summary")
