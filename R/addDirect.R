599d8af03712729659c383f6bcba401c48a2e5a8
Kyle Chung <kylechun9@gmail.com>
2015-10-28 15:41:48 +0800

    add support for directives

---
+++ R/addDirect.R
@@ -0,0 +1,36 @@
+#' @include ArgParser.R
+NULL
+
+#' Add directive to parser.
+#' 
+#' @param x An ArgParser object.
+#' @param name Character vector of the directive name. Should be of length 1.
+#' @param ... Other arguments used in dispatched method.
+
+#' @export
+setGeneric("addDirect", def=function(x, name, ...) standardGeneric("addDirect"))
+
+#' @describeIn addDirect
+#' @param optional Optional logical vector of length 1. Is the directive optional?
+#' @param help Optional character vector shown in usage for the directive. If any, should be of length 1.
+#' @return An ArgParser with the directive definition added.
+#' @examples
+#' p <- ArgParser("a test parser")
+#' p <- addDirect(p, "dosomething")
+
+#' @export
+setMethod("addDirect", signature=c(x="ArgParser", name="character"), 
+          definition=function(x, name, optional=TRUE, help=NULL) {
+              name <- .checkArgLen(name, 1)
+              x@directs <- c(x@directs, name)
+              x@directs_isOptional <- c(x@directs_isOptional, setNames(optional, name))
+              if ( !is.null(help) ) {
+                  help <- .checkArgLen(help, 1)
+                  x@help <- c(x@help, setNames(help, name))
+              }
+              validObject(x)
+              x
+          })
+
+
+
