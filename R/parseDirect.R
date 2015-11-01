d293617ad860d2c660f5b8370bbbc963eb04436d
Kyle Chung <kylechun9@gmail.com>
2015-11-01 20:07:08 +0800

    add support for parsing subflag for directives

--- R/parseDirect.R
+++ R/parseDirect.R
@@ -7,28 +7,40 @@ setMethod("parseDirect", signature=c(x="ArgParser", cmdargs="character"),
               parsed <- list()
 
               if ( length(x@directs) ) {
-                  # check occurrence of forced directives, if any
-                  if ( length(forced_directs <- names(which(!x@directs_isOptional))) ) {
-                      if ( !all(forced_and_raised <- forced_directs %in% cmdargs) )
-                          stop(sprintf("Missing forced directive(s): %s", 
-                                       paste(forced_directs[!forced_and_raised], collapse=", ")))
+                  # check occurrence of forced directive, if any
+                  is_optional <- sapply(x@directs_group, function(x) x$is_optional)
+                  if ( length(forced_dgroups <- names(which(!is_optional))) ) {
+                      for ( dg in forced_dgroups ) {
+                          dnames <- x@directs_group[[dg]]$mem
+                          nraised <- sum(dnames %in% cmdargs)
+                          if ( nraised == 0 ) {
+                              stop(sprintf("Missing forced directive (any of: %s)", 
+                                           paste(dnames, collapse=", ")))
+                          } else if ( nraised > 1 ) {
+                              stop(sprintf("Directives of the same group can not co-exist: %s",
+                                           paste(dnames, collapse=", ")))
+                          }
+                      }
                   }
 
                   # check duplicated directives
-                  .checkDupArg(cmdargs, names(x@directs))
+                  .checkDupArg(cmdargs, all_directs <- names(x@directs))
              
-                  # parse present directives
-                  f1_idx <- match(f1_raised, cmdargs)
-                  parsed <- c(parsed, setNames(cmdargs[f1_idx + 1L], f1_raised))
-                  invalid_value <- parsed %in% c(allargs, NA)
-                  if ( any(invalid_value) )
-                      stop(sprintf("Invalid input in: %s.", 
-                                   paste(names(parsed[invalid_value]), collapse=", ")))
-                  flag_kv_idx <- c(flag_kv_idx, f1_idx, f1_idx + 1L)
+                  # parse present directives along with its sub-commands
+                  raised_direct <- all_directs[all_directs %in% cmdargs]
+                  raised_direct_idx <- match(raised_direct, cmdargs)
+                  pre_cmdargs <- cmdargs[1:(raised_direct_idx-1)]
+                  post_cmdargs <- cmdargs[-(1:raised_direct_idx)]
+                  parsed <- c(parsed, setNames(list(list()), raised_direct))
+                  if ( length(dflags <- x@directs[[raised_direct]]$flags) ) {
+                      fparsed <- parseFlag(x, post_cmdargs, dflags)
+                      parsed[[raised_direct]] <- fparsed$argv
+                      cmdargs <- fparsed$cmdargs_consumed
+                  }
               }
 
               list(argv=parsed, 
-                   cmdargs_consumed=if (length(flag_kv_idx)) cmdargs[-flag_kv_idx] else cmdargs)
+                   cmdargs_consumed=c(pre_cmdargs, cmdargs))
           })
 
 
