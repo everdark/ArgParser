19e099579426e68a912892a16dbb37f13b628466
Kyle Chung <kylechun9@gmail.com>
2015-11-01 17:48:45 +0800

    update unittests to reflect new design of directives

--- tests/testthat/test-addDirect.R
+++ tests/testthat/test-addDirect.R
@@ -5,9 +5,9 @@ context("Add directives onto ArgParser instance")
 
 test_that("name definition of any length can be properly set", {
           expect_identical((ArgParser() %>% addDirect(c("dir1")))@directs,
-                           list(c("dir1")))
+                           list(dir1=NULL))
           expect_identical((ArgParser() %>% addDirect(c("dir1", "dir2")))@directs,
-                           list(c("dir1", "dir2")))
+                           list(dir1=NULL, dir2=NULL))
 })
 
 test_that("duplicated directive name definition will cause error", {
@@ -23,16 +23,16 @@ test_that("duplicated directive group name definition will cause error", {
 })
 
 test_that("optional, if any, is properly set", {
-          expect_identical((ArgParser() %>% addDirect("dir"))@directs_isOptional, 
+          expect_identical((ArgParser() %>% addDirect("dir"))@directs_group$`_g1`$is_optional, 
                            c(FALSE))
-          expect_identical((ArgParser() %>% addDirect("dir", optional=TRUE))@directs_isOptional, 
+          expect_identical((ArgParser() %>% addDirect("dir", optional=TRUE))@directs_group$`_g1`$is_optional, 
                            c(TRUE))
-          expect_identical((ArgParser() %>% addDirect("dir1") %>% addDirect("dir2", optional=T))@directs_isOptional, 
-                           c(FALSE, TRUE))
-          expect_identical((ArgParser() %>% addDirect("dir1", "g1") %>% addDirect("dir2", optional=T))@directs_isOptional, 
-                           c(g1=FALSE, TRUE))
-          expect_identical((ArgParser() %>% addDirect("dir1", "g1") %>% addDirect("dir2", "g2", optional=T))@directs_isOptional, 
-                           c(g1=FALSE, g2=TRUE))
+          expect_identical((ArgParser() %>% addDirect("dir1") %>% addDirect("dir2", optional=T))@directs_group, 
+                           list(`_g1`=list(mem="dir1", is_optional=F), `_g2`=list(mem="dir2", is_optional=T)))
+          expect_identical((ArgParser() %>% addDirect("dir1", "g1") %>% addDirect("dir2", optional=T))@directs_group, 
+                           list(g1=list(mem="dir1", is_optional=F), `_g2`=list(mem="dir2", is_optional=T)))
+          expect_identical((ArgParser() %>% addDirect("dir1", "g1") %>% addDirect("dir2", "g2", optional=T))@directs_group, 
+                           list(g1=list(mem="dir1", is_optional=F), g2=list(mem="dir2", is_optional=T)))
 })
 
 test_that("help, if any, is property set", {
@@ -58,8 +58,8 @@ test_that("help definition of length > length(name) will be warned, and the firs
 
 test_that("all arguments work properly together", {
           p <- ArgParser() %>% addDirect(c("dir1", "dir2"), "dir", F, c("dir1 help", "dir2 help"))
-          expect_identical(p@directs, list(dir=c("dir1", "dir2")))
-          expect_identical(p@directs_isOptional, c(dir=F))
+          expect_identical(p@directs, list(dir1=NULL, dir2=NULL))
+          expect_identical(p@directs_group, list(dir=list(mem=c("dir1", "dir2"), is_optional=F)))
           expect_identical(p@help["dir1"], c(dir1="dir1 help"))
           expect_identical(p@help["dir2"], c(dir2="dir2 help"))
 })
