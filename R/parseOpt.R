516e356fbc17f02b776a6143c01a325b81803888
Kyle Chung <kylechun9@gmail.com>
2015-11-01 20:40:17 +0800

    add support for selective opt parsing

--- R/parseOpt.R
+++ R/parseOpt.R
@@ -1,9 +1,17 @@
 
-setGeneric("parseOpt", def=function(x, cmdargs_consumed) standardGeneric("parseOpt"))
+setGeneric("parseOpt", def=function(x, cmdargs_consumed, ...) standardGeneric("parseOpt"))
 
 setMethod("parseOpt", signature=c(x="ArgParser", cmdargs_consumed="character"),
-          definition=function(x, cmdargs_consumed) {
+          definition=function(x, cmdargs_consumed, limited_to=NULL) {
+
+              if ( !is.null(limited_to) ) {
+                  x@opt <- limited_to
+                  x@opt_narg <- x@opt_narg[limited_to]
+                  x@opt_nrequired <- x@opt_nrequired[limited_to]
+              }
+
               parsed <- list()
+
               if ( length(x@opt) ) {
                   for ( opt in x@opt ) {
                       if ( length(cmdargs_consumed) < x@opt_nrequired[opt] )
@@ -13,6 +21,7 @@ setMethod("parseOpt", signature=c(x="ArgParser", cmdargs_consumed="character"),
                       cmdargs_consumed <- cmdargs_consumed[-to_narg]
                   }
               }
+
               list(argv=parsed,
                    cmdargs_consumed=cmdargs_consumed)
           })
